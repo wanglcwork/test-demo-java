4a5f6ee3b985ffc9d8ecf92cffa3d68747f1304e
wangluochuan <wanglc_work@163.com>
2018-01-22 16:56:15 +0800

    add repository

---
+++ demo/src/main/java/com/luopan/demo/TestCollector.java
@@ -0,0 +1,4 @@
+package com.luopan.demo;
+
+public class TestCollector {
+}
---
+++ demo/src/main/java/com/luopan/demo/TestStream.java
@@ -0,0 +1,15 @@
+package com.luopan.demo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class TestStream {
+    private static Log Logger = LogFactory.getLog(TestStream.class);
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
